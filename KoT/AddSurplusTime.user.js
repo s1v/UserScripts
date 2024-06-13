dabc8b311a750985cf0fe651723da690b849e72d
s1v <somberlain.s1v@gmail.com>
2024-06-13 09:50:17 +0900

    Update AddSurplusTime.user.js

--- KoT/AddSurplusTime.user.js
+++ KoT/AddSurplusTime.user.js
@@ -12,10 +12,10 @@
 (function() {
     'use strict';
 
-    // Your code here...
     const workCount = Number(document.getElementsByClassName('work_count').item(1).innerText); //労働日数
     const kotWorkedTime = document.getElementsByClassName('specific-table_800').item('table').children[1].children[0].children[1].innerText; //労働時間(KoT形式)
 
+    /*労働時間(KoT形式)を労働時間(min)に変換*/
     const part = kotWorkedTime.toString().split('.');
     const workedTime = (Number(part[0]) * 60) + (Number(part[1])); //労働時間(min)
 
@@ -23,16 +23,19 @@
 
     const bonusTime = workedTime - standardWorkTime; //貯金時間(min)
 
+    /*要素の追加先となるHTMLElementを取得*/
     const tableHeadRow = document.evaluate('/html/body/div/div[2]/div/div[4]/table/thead/tr', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null).snapshotItem(0);
     const tableBodyRow = document.evaluate('/html/body/div/div[2]/div/div[4]/table/tbody/tr', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null).snapshotItem(0);
 
+    /*theadの子要素を生成・theadに追加*/
     const additionalHeadColumn = document.createElement('th');
     const additionalHeadItem = document.createElement('p');
-    const headTextNode = document.createTextNode('ボーナスタイム');
+    const headTextNode = document.createTextNode('余剰労働時間');
     additionalHeadItem.appendChild(headTextNode);
     additionalHeadColumn.appendChild(additionalHeadItem);
     tableHeadRow.appendChild(additionalHeadColumn);
 
+    /*tbodyの子要素を生成・tbodyに追加*/
     const additionalBodyColumn = document.createElement('td');
     const bodyTextNode = document.createTextNode(`${bonusTime}分`);
     additionalBodyColumn.appendChild(bodyTextNode);
